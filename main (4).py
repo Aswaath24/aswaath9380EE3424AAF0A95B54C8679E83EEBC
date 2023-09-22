e76f8824018b3de907f1d239609b2f0af6ecd377
Aswaath24 <144251402+Aswaath24@users.noreply.github.com>
2023-09-22 12:22:52 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,30 +1,18 @@
-class BankAccount:
-   def __init__(self,  account_number, account_holder_name, initial_balance=0.0):
-       self.__account_number= account_number
-       self.__account_holder_name= account_holder_name
-       self.__account_balance= initial_balance
+class Student:
 
-   def deposit(self,amount):
-     if amount > 0:
-       #self.__account_balance += amount
-       print("deposited ₹{}. New balance: ₹{}".format(amount,
-          self.__account_balance))
-     else:
-        print("invalid deposit amount. please deposit positive amount")
+ def __init__(self,name,roll_number, cgpa):
+  self. name = name
+  self. roll_number = roll_number
+  self. cgpa = cgpa
 
-   def withdraw(self,amount):
-       if amount > 0 and amount <= self.__account_balance:
-        self.__account_balance -= amount         
-        print ("Withdrawl ₹{}. New balance ₹{}". format(amount, self. __account_balance))
-       else:
-        print("invalid withdrawl account or insufficient balance.")
+def sort_students(students_list):
+   sorted_students = sorted(students_list, key = lambda student: student.cgpa, reverse = True)
 
-   def display_balance(self):
-     print("Account balance {} (Account #{}): ₹{}".format(    self.__account_holder_name, self. __account_number, self.__account_balance))
+   return sorted_students
 
-#create an instance of the BankAccount class
-Account=BankAccount(account_number="1234567890", account_holder_name="Jessy", initial_balance="5000.0") 
+#example
+students = [Student("Hari","A123",7.8) , Student("Srikanth","A124",8.9), Student ("Saranya","A125",6.5),  Student(" Jessy","A126",5.8)      ]
+sorted_students = sort_students(students)
 
-#test the deposit and withdrawal functionality. 
-Account.display_balance()
-Account.deposit(500.0)
+for student in sorted_students:
+  print("Name:{} , Roll Number:{}, CGPA:{}".format(student.name,    student.roll_number, student.cgpa))
